b299906874f6c09c6a051aab6d8acca08d6764da
setedesque <samuel.tedesque@horizontag.com>
2024-08-13 16:06:14 -0300

    feat: Atualizar página do player de vídeo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ nginx -g 'daemon on;'
 ffmpeg -i rtmp://191.252.193.123/live/teste \
     -c:v libx264 -c:a aac -strict -2 -crf 18 -preset veryfast \
     -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename '/usr/share/nginx/html/output%03d.ts' \
-    /usr/share/nginx/html/output.m3u8
+    /usr/share/nginx/html/output.m3u8 &
 
 
 tail -f /dev/null
